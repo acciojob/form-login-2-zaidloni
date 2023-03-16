ab8a988c95c072dc64c3c5a1db5f9a1355a77ce5
Zaid Loni <96578621+zaidloni@users.noreply.github.com>
2023-03-16 14:53:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+ function displayResult() {
+        let firstName = document.getElementById("firstName").value;
+        let lastName = document.getElementById("lastName").value;
+        let phoneNumber = document.getElementById("phoneNumber").value;
+        let emailId = document.getElementById("emailId").value;
+
+        let result = "Name: " + firstName + " " + lastName + "\n" +
+                     "Phone: " + phoneNumber + "\n" +
+                     "Email: " + emailId;
+
+        alert(result);
+      }
